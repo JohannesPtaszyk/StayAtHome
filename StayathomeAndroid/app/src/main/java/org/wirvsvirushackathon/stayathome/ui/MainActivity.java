f70fa7bbf5eb96359f20a1e5ca96d103e2773ec1
Johannes Ptaszyk <johannes@ptaszyk.de>
2020-03-21 13:10:12 +0100

    ADD Android project

---
+++ StayathomeAndroid/app/src/main/java/org/wirvsvirushackathon/stayathome/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package org.wirvsvirushackathon.stayathome.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.wirvsvirushackathon.stayathome.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
