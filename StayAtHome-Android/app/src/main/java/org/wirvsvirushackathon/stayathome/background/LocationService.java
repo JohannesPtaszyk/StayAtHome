f9693b2ed93c4e276e8c6e35f1fd493ad35b9baa
Sean Noran <sean.noran@exxcellent.de>
2020-03-21 16:37:01 +0100

    feat(location): persist location in shared preferences

--- StayAtHome-Android/app/src/main/java/org/wirvsvirushackathon/stayathome/background/LocationService.java
+++ StayAtHome-Android/app/src/main/java/org/wirvsvirushackathon/stayathome/background/LocationService.java
@@ -7,6 +7,7 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -89,28 +90,12 @@ public class LocationService extends Service {
                     getBaseContext(),
                     "Location changed: Lat: " + loc.getLatitude() + " Lng: "
                             + loc.getLongitude(), Toast.LENGTH_SHORT).show();
-            final String longitude = "Longitude: " + loc.getLongitude();
-            Log.v(TAG, longitude);
-            String latitude = "Latitude: " + loc.getLatitude();
-            Log.v(TAG, latitude);
 
-            /*------- To get city name from coordinates -------- */
-            String cityName = null;
-            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
-            List<Address> addresses;
-            try {
-                addresses = gcd.getFromLocation(loc.getLatitude(),
-                        loc.getLongitude(), 1);
-                if (addresses.size() > 0) {
-                    System.out.println(addresses.get(0).getLocality());
-                    cityName = addresses.get(0).getLocality();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
-                    + cityName;
+            final SharedPreferences sharedPreferences = getSharedPreferences("location_log", MODE_PRIVATE);
+            sharedPreferences.edit()
+                    .putFloat("last_latitude", (float) loc.getLatitude())
+                    .putFloat("last_longitude", (float) loc.getLongitude())
+                    .apply();
         }
 
         @Override
